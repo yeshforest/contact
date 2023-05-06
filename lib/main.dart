56ab7cd482ee885ca508427f96e1d22f60796a4d
haerimRoh <mtxpvksh@gmail.com>
2023-05-06 21:43:05 +0900

    [feat] Expanded example

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //50lp=1.2cm
         home: Scaffold(
-      appBar: AppBar(title: Text('앱임')),
-      body: Text('안녕'),
-      bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height:100,
-            child: Row(
+            appBar: AppBar(
 
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+            body: Container(
+              height: 150,
+              padding: EdgeInsets.all(10),
+              child: Row(
                 children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page)
-            ]),
-          )),
-    ));
+                  Container(
+                      child: Image.asset(
+                          '/Users/hr.r/AndroidStudioProjects/contact/assets/images/clover.jpeg',
+                          width: 150)),
+                  Expanded(
+                      child: Container(
+                          child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '캐논 DSLR 100D(단렌즈, 충전기 16기가SD포함)',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ),
+                      Text(
+                        '금호동3가 끌올 10분 전',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text(
+                        '100000원',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                        Icon(Icons.favorite_border_outlined),
+                        Text('4')
+                      ]),
+                    ],
+                  )))
+                ],
+              ),
+            )));
   }
 }
+//함수형이 아니고 객체지향이라 리엑트 쓰다보니 객체지향 쓰니까 재밌다.
